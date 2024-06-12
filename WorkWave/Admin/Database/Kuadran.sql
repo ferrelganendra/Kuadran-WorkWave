a7db7efc7b3677004a15878e86af7d78a8a0e84a
dezasters <111865647+Aleale0612@users.noreply.github.com>
2024-06-12 15:04:08 +0700

    Add files via upload

--- WorkWave/Admin/Database/Kuadran.sql
+++ WorkWave/Admin/Database/Kuadran.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 22, 2024 at 03:00 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Waktu pembuatan: 09 Jun 2024 pada 09.38
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Struktur dari tabel `admin`
 --
 
 CREATE TABLE `admin` (
@@ -35,10 +35,18 @@ CREATE TABLE `admin` (
   `Nama` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`IDAdmin`, `Username`, `Password`, `Email`, `Nama`) VALUES
+(1, 'Rakha', '12345', 'Rakha@gmail.com', 'Rakha'),
+(3, 'Admin', 'Admin', 'Admin@gmail.com', 'Admin');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `event`
+-- Struktur dari tabel `event`
 --
 
 CREATE TABLE `event` (
@@ -59,7 +67,7 @@ CREATE TABLE `event` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kategori_pekerjaan`
+-- Struktur dari tabel `kategori_pekerjaan`
 --
 
 CREATE TABLE `kategori_pekerjaan` (
@@ -68,7 +76,7 @@ CREATE TABLE `kategori_pekerjaan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `kategori_pekerjaan`
+-- Dumping data untuk tabel `kategori_pekerjaan`
 --
 
 INSERT INTO `kategori_pekerjaan` (`id`, `nama_kategori`) VALUES
@@ -88,7 +96,7 @@ INSERT INTO `kategori_pekerjaan` (`id`, `nama_kategori`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `loker`
+-- Struktur dari tabel `loker`
 --
 
 CREATE TABLE `loker` (
@@ -101,13 +109,25 @@ CREATE TABLE `loker` (
   `status_kerja` enum('Full-time','Part-time','Kontrak') NOT NULL,
   `besaran_gaji` decimal(10,2) NOT NULL,
   `lokasi_bekerja` varchar(255) NOT NULL,
-  `syarat_pekerjaan` text NOT NULL
+  `syarat_pekerjaan` text NOT NULL,
+  `tanggal_dipost` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `loker`
+--
+
+INSERT INTO `loker` (`id`, `user_id`, `kategori_pekerjaan_id`, `posisi`, `tingkat_pendidikan`, `gender`, `status_kerja`, `besaran_gaji`, `lokasi_bekerja`, `syarat_pekerjaan`, `tanggal_dipost`) VALUES
+(1, 1, 12, 'asdca', 'ewfaw', 'Laki-laki', 'Part-time', 10.00, 'vawev', 'gergerg\r\nesrgserg\r\nersgerg\r\nsergserg\r\nsergesrg\r\nsergser', NULL),
+(2, 1, 12, 'asdca', 'ewfaw', 'Laki-laki', 'Part-time', 10.00, 'vawev', 'gergerg\r\nesrgserg\r\nersgerg\r\nsergserg\r\nsergesrg\r\nsergser', NULL),
+(10, 17, 1, 'rfw', 'asdf', 'Laki-laki', '', 32.00, 'weqf', 'adswf', NULL),
+(11, 17, 3, 'Guru Sekolah Dasar', 'S1', 'Perempuan', '', 3000000.00, 'Sd Negeri 001 Sleman', '1. Pendidikan minimal S1\r\n2. Bisa mengajar anak autis\r\n3. Ini Prankk', NULL),
+(12, 17, 1, 'dsgaf', 'sadf', 'Perempuan', '', 234.00, 'wer', 'asfd', '2024-06-18');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -125,23 +145,52 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `nama_perusahaan`, `industri`, `deskripsi_perusahaan`, `media_sosial`, `website`, `alamat_perusahaan`, `logo_perusahaan`, `username`, `password`, `status`) VALUES
+(1, 'savero', 'sasasa', 'ksjabiula fwuipegf iuaw efg pewiua gfpeiwu gfupif gwipfg aiuyefg aliuwyoe fla wei fol gaewf', 'ig:verzzkh', '-', 'dsadfuiwha fpuwaeghfpiuw efg paiuyg fipuaWE Y AW', '', '12345', '12345', 'diterima'),
+(2, 'savero', 'sasa', 'uyigefuia ewrfiauwg fpiauwygef ipawug friapuyfg aliuweyrfugy wer fa wf', 'ig:verzzkh', '-', 'hgdfalisy fgagf ayufg olayf gayw gf', 'sdd', '12345', '12345', 'ditolak'),
+(3, 'gugug', 'arsgaergf', 'aerwgawg', 'weagaweg', 'ewagaw', 'geawgwa', 'awegwegw', 'agus', 'agus', 'menunggu'),
+(4, '', '', '', '', '', '', '', '', '', 'menunggu'),
+(5, '', '', '', '', '', '', '', '', '', 'menunggu'),
+(6, '', '', '', '', '', '', '', 'sa', 'sekar', 'diterima'),
+(7, '', '', '', '', '', '', '', 'asd', 'asd', 'menunggu'),
+(8, '', '', '', '', '', '', '', 'aaa', 'aaaaaaaa', 'menunggu'),
+(9, '', '', '', '', '', '', '', 'as', 'ssssssss', 'menunggu'),
+(10, 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', '', '', 'diterima'),
+(11, '', '', '', '', '', '', '', '', '', 'menunggu'),
+(12, 'dsf', 'sadfsa', 'dfsdaf', 'sadfsda', 'fasdfas', 'dfsa', 'asdfas', 'aaa', 'aaa', 'menunggu'),
+(13, '', '', '', '', '', '', '', '', '', 'menunggu'),
+(14, '', '', '', '', '', '', '', '', '', 'menunggu'),
+(15, 'sadfasdf', 'sadfasdfa', 'sdfsdf', 'sdfasd', 'fasdfasd', 'fsafasd', 'dsafas', 'wawa', 'wawa', 'diterima'),
+(16, 'swsw', 'asdx', 'asda', 'sdasda', 'dasdasd', 'asdas', 'dasdsad', 'asd', 'asd', 'menunggu'),
+(17, 'gustui', 'jrytdt', 'ghjfjht', 'kutdct', 'iyflyf', 'jgck,ujy', 'ily7lf', 'aaaa', 'aaaa', 'diterima');
+
+--
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `event`
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`IDAdmin`);
+
+--
+-- Indeks untuk tabel `event`
 --
 ALTER TABLE `event`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `kategori_pekerjaan`
+-- Indeks untuk tabel `kategori_pekerjaan`
 --
 ALTER TABLE `kategori_pekerjaan`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `loker`
+-- Indeks untuk tabel `loker`
 --
 ALTER TABLE `loker`
   ADD PRIMARY KEY (`id`),
@@ -149,45 +198,45 @@ ALTER TABLE `loker`
   ADD KEY `FK_kategori_pekerjaan_loker` (`kategori_pekerjaan_id`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `event`
+-- AUTO_INCREMENT untuk tabel `event`
 --
 ALTER TABLE `event`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `kategori_pekerjaan`
+-- AUTO_INCREMENT untuk tabel `kategori_pekerjaan`
 --
 ALTER TABLE `kategori_pekerjaan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT for table `loker`
+-- AUTO_INCREMENT untuk tabel `loker`
 --
 ALTER TABLE `loker`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `loker`
+-- Ketidakleluasaan untuk tabel `loker`
 --
 ALTER TABLE `loker`
   ADD CONSTRAINT `FK_kategori_pekerjaan_loker` FOREIGN KEY (`kategori_pekerjaan_id`) REFERENCES `kategori_pekerjaan` (`id`) ON DELETE CASCADE,
--- WorkWave/users/dashboardEvent.php
+++ WorkWave/users/dashboardEvent.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Events</title>
+    <title>Job Fair Lokal</title>
     <link rel="stylesheet" href="css/StEvent.css">
     <script src="https://unpkg.com/scrollreveal"></script>
     <script src="js/Scriptevents.js"></script>
