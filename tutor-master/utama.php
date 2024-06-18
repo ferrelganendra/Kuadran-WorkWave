b6745f2940945f9ba4ccb4c8c0ab4ad6f4bc7c6b
Ferrel Ganendra <ferrelganendra25@gmail.com>
2024-06-18 22:30:09 +0700

    .

--- tutor-master/utama.php
+++ tutor-master/utama.php
@@ -68,7 +68,8 @@ $categories = $result->fetch_all(MYSQLI_ASSOC);
 
               <nav class="site-navigation text-right ml-auto d-none d-lg-block" role="navigation">
                 <ul class="site-menu main-menu js-clone-nav ml-auto ">
-                  <li class="active"><a href="utama.html" class="nav-link">Utama</a></li> 
+                  <li class="active"><a href="utama.html" class="nav-link">Utama</a></li>
+                  <li><a href="lowongan.html" class="nav-link">Lowongan</a></li>
                   <li><a href="grafik.php" class="nav-link">Grafik</a></li>
                   <li><a href="bursakerja.html" class="nav-link">Bursa Kerja</a></li>
                   <li><a href="registrasi.html" class="nav-link">Registrasi</a></li>
