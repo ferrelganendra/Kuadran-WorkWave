ffde44631262ab2fff794f1796a9f92a75bdfef4
dezasters <111865647+Aleale0612@users.noreply.github.com>
2024-07-03 02:59:14 +0700

    Add files via upload

--- tutor-master/php/Admin/prosesLogin.php
+++ tutor-master/php/Admin/prosesLogin.php
@@ -13,65 +13,93 @@ if (isset($_POST['login'])) {
     // Check if username and password are not empty
     if (!empty($username) && !empty($password)) {
         // Prepare and bind
-        $stmt = $koneksi->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
-        $stmt->bind_param("ss", $username, $password);
+        $stmt = $koneksi->prepare("SELECT * FROM users WHERE username = ?");
+        $stmt->bind_param("s", $username);
         $stmt->execute();
         $result = $stmt->get_result();
 
         if ($result->num_rows == 1) {
             $user_data = $result->fetch_assoc();
-            $status = $user_data['status'];
-            $package_purchased = $user_data['package_purchased'];
+            if (password_verify($password, $user_data['password'])) {
+                $status = $user_data['status'];
+                $package_purchased = $user_data['package_purchased'];
 
-            // Check user status
-            if ($status == 'diterima') {
-                // Set session
-                $_SESSION['username'] = $username;
-                $_SESSION['user_id'] = $user_data['id']; // Store user ID in session
-                $_SESSION['status'] = $status;
-                $_SESSION['package_purchased'] = $package_purchased;
+                // Check user status
+                if ($status == 'diterima') {
+                    // Set session
+                    $_SESSION['username'] = $username;
+                    $_SESSION['user_id'] = $user_data['id']; // Store user ID in session
+                    $_SESSION['status'] = $status;
+                    $_SESSION['package_purchased'] = $package_purchased;
 
-                // SweetAlert script
-                echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js'></script>";
-                echo "<script>
-                        document.addEventListener('DOMContentLoaded', function() {
-                          Swal.fire({
-                            title: 'Success!',
-                            text: 'Jangan lupa menambahkan Logo Perusahaan ya!',
-                            icon: 'success',
-                            confirmButtonText: 'OK'
-                          }).then((result) => {
-                            if (result.isConfirmed) {
-                              window.location.href = '../../profil.php?status=diterima&user_id=" . $_SESSION['user_id'] . "&package_purchased=" . $_SESSION['package_purchased'] . "';
-                            }
-                          });
-                        });
-                      </script>";
-                exit();
-            } elseif ($status == 'menunggu') {
-                // Alert 1 "menunggu"
-                echo "<script>
-                        document.addEventListener('DOMContentLoaded', function() {
-                          Swal.fire({
-                            title: 'Info',
-                            text: 'Akun Anda sedang dalam proses verifikasi admin. Harap tunggu konfirmasi dari admin.',
-                            icon: 'info',
-                            confirmButtonText: 'OK'
-                          }).then((result) => {
-                            if (result.isConfirmed) {
-                              window.location.href = '../../login.php';
-                            }
-                          });
-                        });
-                      </script>";
-                exit();
-            } elseif ($status == 'ditolak') {
-                // Alert 2 "ditolak"
+                    // SweetAlert script
+                    echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js'></script>";
+                    echo "<script>
+                            document.addEventListener('DOMContentLoaded', function() {
+                              Swal.fire({
+                                title: 'Success!',
+                                text: 'Jangan lupa menambahkan Logo Perusahaan ya!',
+                                icon: 'success',
+                                confirmButtonText: 'OK'
+                              }).then((result) => {
+                                if (result.isConfirmed) {
+                                  Swal.fire({
+                                    title: 'Peringatan',
+                                    text: 'Beli paket lowongan pekerjaan agar dapat mengupload lowongan.',
+                                    icon: 'warning',
+                                    confirmButtonText: 'OK'
+                                  }).then((result) => {
+                                    if (result.isConfirmed) {
+                                      window.location.href = '../../profil.php?status=diterima&user_id=" . $_SESSION['user_id'] . "&package_purchased=" . $_SESSION['package_purchased'] . "';
+                                    }
+                                  });
+                                }
+                              });
+                            });
+                          </script>";
+                    exit();
+                } elseif ($status == 'menunggu') {
+                    // Alert 1 "menunggu"
+                    echo "<script>
+                            document.addEventListener('DOMContentLoaded', function() {
+                              Swal.fire({
+                                title: 'Info',
+                                text: 'Akun Anda sedang dalam proses verifikasi admin. Harap tunggu konfirmasi dari admin.',
+                                icon: 'info',
+                                confirmButtonText: 'OK'
+                              }).then((result) => {
+                                if (result.isConfirmed) {
+                                  window.location.href = '../../login.php';
+                                }
+                              });
+                            });
+                          </script>";
+                    exit();
+                } elseif ($status == 'ditolak') {
+                    // Alert 2 "ditolak"
+                    echo "<script>
+                            document.addEventListener('DOMContentLoaded', function() {
+                              Swal.fire({
+                                title: 'Error',
+                                text: 'Akun Anda tidak lolos verifikasi admin. Silakan hubungi admin untuk informasi lebih lanjut.',
+                                icon: 'error',
+                                confirmButtonText: 'OK'
+                              }).then((result) => {
+                                if (result.isConfirmed) {
+                                  window.location.href = '../../login.php';
+                                }
+                              });
+                            });
+                          </script>";
+                    exit();
+                }
+            } else {
+                // Alert 3
                 echo "<script>
                         document.addEventListener('DOMContentLoaded', function() {
                           Swal.fire({
                             title: 'Error',
-                            text: 'Akun Anda tidak lolos verifikasi admin. Silakan hubungi admin untuk informasi lebih lanjut.',
+                            text: 'Username dan password salah!',
                             icon: 'error',
                             confirmButtonText: 'OK'
                           }).then((result) => {
--- tutor-master/php/Admin/prosesRegis.php
+++ tutor-master/php/Admin/prosesRegis.php
@@ -13,6 +13,9 @@ if (isset($_POST['register'])) {
     $alamat_perusahaan = $_POST['alamat'];
     $username = $_POST['username'];
     $password = $_POST['password'];
+
+    // Hash the password
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
     
     // Set status default 'menunggu'
     $status = 'menunggu';
@@ -20,14 +23,27 @@ if (isset($_POST['register'])) {
     $query = "INSERT INTO users (nama_perusahaan, industri, deskripsi_perusahaan, media_sosial, website, alamat_perusahaan, username, password, status) 
               VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $stmt = $koneksi->prepare($query);
-    $stmt->bind_param('sssssssss', $nama_perusahaan, $industri, $deskripsi_perusahaan, $media_sosial, $website, $alamat_perusahaan, $username, $password, $status);
+    $stmt->bind_param('sssssssss', $nama_perusahaan, $industri, $deskripsi_perusahaan, $media_sosial, $website, $alamat_perusahaan, $username, $hashed_password, $status);
 
     if ($stmt->execute()) {
         // Set session
         $_SESSION['username'] = $username;
-        // Menggunakan header location untuk mengarahkan ke profil.php
-        echo "<script>alert('Akun Anda sedang dalam proses verifikasi admin. Harap tunggu konfirmasi dari admin.'); window.location.href = '../../login.php';</script>";
-        header("Location: ../../login.php");
+        // Menggunakan header location untuk mengarahkan ke login.php
+        echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js'></script>";
+        echo "<script>
+                document.addEventListener('DOMContentLoaded', function() {
+                  Swal.fire({
+                    title: 'Success!',
+                    text: 'Akun Anda sedang dalam proses verifikasi admin. Harap tunggu konfirmasi dari admin.',
+                    icon: 'success',
+                    confirmButtonText: 'OK'
+                  }).then((result) => {
+                    if (result.isConfirmed) {
+                      window.location.href = '../../login.php';
+                    }
+                  });
+                });
+              </script>";
         exit();
     } else {
         echo "Error: " . $stmt->error;
@@ -36,3 +52,4 @@ if (isset($_POST['register'])) {
     $stmt->close();
 }
 ?>
+
